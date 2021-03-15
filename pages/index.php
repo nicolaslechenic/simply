675ad60b6ff6ebfb116118ca14902dc8319da4e0
unknown <yarolsav.hontar@gmail.com>
2021-03-15 21:00:13 +0100

    first commit

---
+++ pages/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+  </script>
+<script src="https://kit.fontawesome.com/761d77ec55.js" crossorigin="anonymous"></script>
+  <title>Simply Home</title>
+</head>
+
+<body class="conatiner-fluid">
+  <header>
+    <div id="top-nav">
+      <div class="container">
+            <ul class="nav d-none d-sm-flex">
+              <li class="nav-item">
+                <span>|</span>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://facebook.com"><i class="fab fa-facebook-f"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://linkedin.com"><i class="fab fa-linkedin-in"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+              </li>
+              <li class="nav-item">
+                <span>|</span>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://instagram.com"><i class="fas fa-phone-alt"></i> 02 57 87 71 85</a>
+              </li>
+              <li class="nav-item">
+                <span>|</span>
+              </li>
+    </div>
+  </header>
+
+</body>
+
+</html>
