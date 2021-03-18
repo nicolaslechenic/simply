d1777243d10f71cc7e97990a70dd43c3177ed2ab
galba-rp <yaroslav.hontar@gmail.com>
2021-03-18 10:28:39 +0100

    home page finished

--- pages/accueil.php
+++ pages/accueil.php
@@ -49,11 +49,12 @@ $current=basename($path);
             <span>|</span>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="https://instagram.com"><i class="fas fa-phone-alt"></i> 02 57 87 71 85</a>
+            <a class="nav-link" href="#"><i class="fas fa-phone-alt"></i> 02 57 87 71 85</a>
           </li>
           <li class="nav-item" data-toggle="modal" data-target="#myModal">
-            <span>| Suivre mon projet |</span>
+            <span>| <i class="fas fa-scroll"></i> Suivre mon projet |</span>
           </li>
+        </ul>
       </div>
       <!-- The Modal -->
       <div class="modal" id="myModal">
@@ -81,7 +82,7 @@ $current=basename($path);
     <div class="container-fluid d-flex align-items-end" id="header-main-nav">
       <div class="container">
         <nav class="navbar navbar-expand-md navbar-dark">
-          <a class="navbar-brand d-flex" href="home.php">
+          <a class="navbar-brand d-flex" href="accueil.php">
             <img src="../images/simply-home-logo.png" alt="logo">
             <div>
               <p class="mb-0 mt-1">Simply Home</p>
@@ -232,34 +233,54 @@ $current=basename($path);
   </main>
   <footer class="mt-3 d-flex justify-content-around">
     <img src="../images/img-bg-footer.jpg" alt="large image">
-    <div class="container d-flex justify-content-between p-5" id="footer-main">
-      <div id="adresse">
+    <div class="container d-flex justify-content-between p-lg-5 p-md-1" id="footer-main">
+      <div class="adresse text-center">
         <i class="fas fa-map-marker-alt"></i>
         <p class="bold mt-2 mb-0">Simply Home</p>
         <p class="bold mt-0 mb-0">15 rue des Oliviers</p>
         <p class="bold">16100 ANGOULEME</p>
         <p>
-        <a href="tel:00000000000" class="bold">02 57 87 71 85</a>
-      </p>
+          <a href="tel:00000000000" class="bold mt-lg-3 mt-md-1">02 57 87 71 85</a>
+        </p>
       </div>
-      <div id="footer-black" class="d-flex justify-content-center align-items-center">
-        <img src="../images/simply-home-logo.png" alt="logo">
-        <div class="text-left">
-          <p class="bold mb-0">Simply Home</p>
-          <p class="bold">Constructeur de vie</p>
+      <div id="footer-black" class="d-flex flex-column justify-content-around align-items-center">
+        <div id="black-top" class="d-flex justify-content-center align-items-center">
+          <img src="../images/simply-home-logo.png" alt="logo">
+          <div class="text-left">
+            <p class="bold mb-0">Simply Home</p>
+            <p class="bold">Constructeur de vie</p>
+          </div>
+        </div>
+        <div id="black-social">
+          <ul class="nav d-none d-sm-flex justify-content-center mt-lg-4 mt-md-1">
+            <li class="nav-item">
+              <a class="nav-link" href="https://facebook.com"><i class="fab fa-facebook-f"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://linkedin.com"><i class="fab fa-linkedin-in"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div id="adresse">
-        <i class="fas fa-map-marker-alt"></i>
-        <p class="bold mt-2 mb-0">Simply Home</p>
-        <p class="bold mt-0 mb-0">15 rue des Oliviers</p>
-        <p class="bold">16100 ANGOULEME</p>
+      <div class="adresse text-center mt-1" id="inscription">
+        <i class="far fa-paper-plane"></i>
+        <p class="bold mt-2 mb-0">Inscription</p>
+        <p class="bold mt-0 mb-0">a la Newsletter</p>
         <p>
-        <a href="tel:00000000000" class="bold">02 57 87 71 85</a>
-      </p>
+          <button type="button" class="bold px- lg-5 px-md-2 py-2 mt-3">Je m'incris</button>
+        </p>
       </div>
     </div>
-
+    <div id="footer-bottom" class="container-fluid d-flex justify-content-center pt-2 ">
+      <p class="bold">Copyright CDA 2021 - <a href="https://policies.google.com/terms?hl=fr" class="bold">Mentions
+          Légales</a></p>
+    </div>
   </footer>
 </body>
 
